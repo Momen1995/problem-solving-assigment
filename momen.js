4426d43e92fc9d994135dc711904b12c54d5b667
Momen1995 <momensrk@gmail.com>
2024-05-30 20:54:41 +0600

    3 question solve

--- momen.js
+++ momen.js
@@ -2,3 +2,20 @@
 
 let carName = "Volvo";
 console.log(carName)
+
+//2
+let firstName = "John",
+    lastName = "Doe",
+    age = 35;
+
+console.log(firstName,lastName,age)
+
+/* 3. Use the correct assignment operator that will 
+result in x being 50 (same as x = x * y).  */
+
+let x = 10;
+let y = 5;
+
+x *= y;
+console.log(x);
+
