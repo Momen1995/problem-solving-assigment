75b9161b58314119900a6eaf05766ff0997035f4
Momen1995 <momensrk@gmail.com>
2024-05-31 21:38:15 +0600

    comment add

--- momen.js
+++ momen.js
@@ -2,40 +2,47 @@
 let carName = "Volvo";
 
 //2
-let firstName = "John",
+/* let firstName = "John",
     lastName = "Doe",
-    age = 35;
+    age = 35; */
 
 
 // 3.
-let x = 10;
-let y = 5;
-x *= y; // Ans: 50
+// let x = 10;
+// let y = 5;
+// x *= y; // Ans: 50
+
+// console.log(x)
 
 
 // 4.
-let length = 15 //number
-let lastName1 = "Johnshon"; //String
-const x = {
-  firstName: "John",  
-  lastName: "Doe"
-};    // object 
+// let length = 15 //number
+// let lastName = "Johnshon"; //String
+// const x = {
+//   firstName: "John",  
+//   lastName: "Doe"
+// };    // object 
 
+// console.log(typeof length)
+// console.log(typeof length)
+// console.log(typeof length)
 
 
 //5.
-function myFunction() {
-   alert("Hello World!");
-}
-myFunction()
+// function myFunction() {
+//    alert("Hello World!");
+// }
+// myFunction()
+
 
 //6
-const person = {
-  name : "John",
-  age : 50,
-}
+// const person = {
+//   name : "John",
+//   age : 50,
+// }
 
-alert(`${person.name} is ${person.age}`)
+// alert(`${person.name} is ${person.age}`)
+
 
 
 //7. The <button> element should do something when someone clicks on it. Try to fix it!
@@ -44,42 +51,47 @@ alert(`${person.name} is ${person.age}`)
 
 
 //8 i.
-const cars = ["Volvo", "Jeep", "Mercedes"];
-alert(`There are ${cars.length} item in this array`)
+// const cars = ["Volvo", "Jeep", "Mercedes"];
+// alert(`There are ${cars.length} item in this array`);
+
 
 //8 ii.
-const Brand = ["Volvo", "Jeep", "Mercedes"]; 
-Brand[0] = "Ford";
+// const Brand = ["Volvo", "Jeep", "Mercedes"]; 
+// Brand[0] = "Ford";
 
 
 //9.i --> 1 to 10 random numbers
-const randomNum = Math.trunc(Math.random() * 10 + 1);
-console.log(randomNum)
+// const randomNum = Math.trunc(Math.random() * 10 + 1);
+// console.log(randomNum);
+
 
 //9.ii
-function largestNum(n1,n2){
-  const largeNum = Math.max(n1,n2);
-  return largeNum;
-}
-console.log(largestNum(10,20))
+// function largestNum(n1,n2){
+//   const largeNum = Math.max(n1,n2);
+//   return largeNum;
+// }
+// console.log(largestNum(10,20));
+
 
 //9.iii
-console.log(Math.sqrt(9))
+// console.log(Math.sqrt(9))
+
 
 //10.i
-x = 10;
-y = 5;
+// const x = 10;
+// const y = 5;
 
-alert(x > y);
+// alert(x > y);
+
 
 //10.ii
-function ageCalc(age){
-  const ageFind = age < 18 ? alert("Too young") : alert("Old enough");
-  return ageFind;
-}
+// function ageCalc(age){
+//   const ageFind = age < 18 ? alert("Too young") : alert("Old enough");
+//   return ageFind;
+// }
 
-console.log(ageCalc(17));
-console.log(ageCalc(20));
+// console.log(ageCalc(17));
+// console.log(ageCalc(20));
 
 
 
